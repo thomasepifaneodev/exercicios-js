ff401b055e42f80aa3da4f180a3054372e6bd242
Thomás Epifâneo <thomasepifaneodev@outlook.com.br>
2024-01-24 17:14:57 -0300

    Parou na aula 43

---
+++ fundamentos/numerosCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0);
+console.log("10,2" / 2);
+console.log("10" / 2);
+console.log("Show" * 2);
+console.log(0.2 + 0.1);
+console.log(1 / 2);
--- fundamentos/tipaegemFraca.js
+++ fundamentos/tipaegemFraca.js
@@ -4,9 +4,11 @@ console.log(typeof qualquer);
 qualquer = 3.14
 console.log(typeof qualquer);
 
-//Evitar nome genéricos para variáveis
+//Evitar nome genéricos para variáveis.
 let numero = 1;
 let valor = '---';
 let pqp = false;
 
 console.log(numero, valor, pqp);
+
+//Utilizar const/let sempre que puder.
